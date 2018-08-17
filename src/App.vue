dd2eaa9f4e46be0499e83560e4895b359f3a73ae
YuChengKai <597813039@qq.com>
2018-08-17 21:05:09 +0800

    example input

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="box">
+      <e-input placeholder="1"></e-input>
+    </div>
+    <div class="box">
+      <e-input placeholder="1" disabled></e-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import EInput from "../package/input/input";
+export default {
+  components: { EInput }
+};
+</script>
+
+<style>
+.box {
+  margin: 20px;
+}
+</style>
